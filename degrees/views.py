0dea60796cca94b0559b61a2df14226834824c60
Conner Reeves <connerreeves@gmail.com>
2012-04-03 15:50:49 -0700

    Fixed sign in indent issue

--- degrees/views.py
+++ degrees/views.py
@@ -1,1 +1,5 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from courses.models import Section
+
+def preview(request,id):
+	return render_to_response('preview.html', {})
