cefce6fc4a3f42373156a00ce9909731ce05aae6
Conner Reeves <connerreeves@gmail.com>
2012-04-22 22:28:08 -0700

    Set up framework for degree authorship.

--- administrator/urls.py
+++ administrator/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 
-urlpatterns = patterns('student.views',
+urlpatterns = patterns('administrator.views',
+    url(r'^create_degree', 'create_degree'),
+    url(r'^edit_degree', 'edit_degree'),
+    url(r'^create_admin', 'create_admin'),
 )
--- administrator/views.py
+++ administrator/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from django.shortcuts import redirect
+from gradpath import render_to, get_profile
+from gradpath.courses.models import Course
+from gradpath.profiles.models import Record
+
+def create_degree(request):
+    return render_to(request, 'administrator/create_degree.html', { 'key': 'val' })
+
+def edit_degree(request):
+    return render_to(request, 'administrator/edit_degree.html', { 'key': 'val' })
+
+def create_admin(request):
+    return render_to(request, 'administrator/create_admin.html', { 'key': 'val' })
