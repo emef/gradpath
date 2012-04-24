1ca0cc663709716f118fc3ffe0d3345dba23020a
Conner Reeves <connerreeves@gmail.com>
2012-04-24 00:20:18 -0700

    Degree XML file creation working.

--- administrator/urls.py
+++ administrator/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('administrator.views',
-    url(r'^create_degree', 'create_degree'),
+    url(r'^open_degree', 'open_degree'),
     url(r'^edit_degree', 'edit_degree'),
     url(r'^create_admin', 'create_admin'),
 )
--- administrator/views.py
+++ administrator/views.py
@@ -1,13 +1,38 @@
 from django.shortcuts import redirect
 from gradpath import render_to, get_profile
-from gradpath.courses.models import Course
-from gradpath.profiles.models import Record
+from gradpath.courses.models import Section
+from gradpath.settings import XML_PATH
+import datetime,sys
 
-def create_degree(request):
-    return render_to(request, 'administrator/create_degree.html', { 'key': 'val' })
+def open_degree(request):
+    return render_to(request, 'administrator/open_degree.html', {
+    	'years': range(2000, datetime.datetime.now().year + 5)
+    })
 
 def edit_degree(request):
-    return render_to(request, 'administrator/edit_degree.html', { 'key': 'val' })
+	if request.method == 'POST':
+		section = request.POST.get('section', None).lower()
+		type = request.POST.get('type', None).lower()
+		year = request.POST.get('year', None)
+		filename = str(section + '_' + type + '_' + year + '.xml').replace(' ','_')
+		print filename
+		if section and type and year:
+			##Open the XML file
+			try:
+				xml_file = open(XML_PATH + '/' + filename, 'r')
+			except:
+				xml_file = open(XML_PATH + '/' + filename, 'w')
+
+				xml_file.write('<xml>\n<degree section="' + section + '" type="' + type + '" year="' + year + '">\n</degree>\n</xml>')
+
+			##Render the results
+
+			return render_to(request, 'administrator/edit_degree.html', {
+				'section': section.title(),
+				'type': type.title(),
+				'year': year
+			})
+	return redirect('/administrator/open_degree/')
 
 def create_admin(request):
     return render_to(request, 'administrator/create_admin.html', { 'key': 'val' })
