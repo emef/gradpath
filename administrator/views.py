131aa725e1ff579a7b2bf4af38ee107105ecd6ef
Conner Alex Reeves <reevesc3@CF498-xx.cs.wwu.edu>
2012-05-22 13:52:05 -0700

    Made a bunch of visual changes and added the create admin page.

--- administrator/views.py
+++ administrator/views.py
@@ -1,4 +1,5 @@
 from django.http import Http404
+from django.contrib.auth.models import User
 from django.shortcuts import redirect
 from gradpath import render_to, contains, extract, json_response
 from gradpath.courses.models import Section, Course
@@ -56,11 +57,58 @@ def edit_degree(request, id):
         raise Http404
         
     return render_to(request, 'administrator/edit_degree.html', {
-    	'degree': degree,
+        'degree': degree,
     })
 
 def create_admin(request):
-    return render_to(request, 'administrator/create_admin.html', { 'key': 'val' })
+    output = {}
+    if request.user.is_authenticated() and request.user.is_superuser:
+        if request.method == 'POST':
+            errors = {}
+        
+            # extract values from POST, will be None if not present
+            username, pw1, pw2 = extract(request.POST, 'username', 'password1', 'password2')
+        
+            # begin validation...
+            if username is None or len(username) == 0:
+                errors['username'] = 'Required'
+            else:
+                try:
+                    user = User.objects.get(username=username)
+                    #user.delete()
+                    errors['username'] = 'That universal login was already registered.'
+                except User.DoesNotExist:
+                    # good
+                    pass
+
+            if pw1 is None or len(pw1) == 0:
+                errors['password1'] = 'Required'
+
+            if pw2 is None  or len(pw2) == 0:
+                errors['password2'] = 'Required'
+
+            if pw1 != pw2:
+                errors['password1'] = 'Passwords must match'
+
+            # ... end of validation
+            
+            if len(errors) == 0:
+                user = User(username=username)
+                user.set_password(pw1)
+                user.is_active = True
+                user.is_superuser = True
+                user.is_staff = True
+                user.save()
+                
+                return render_to(request, 'administrator/register_success.html', {
+                	'username':username
+                })
+                
+            output['errors'] = errors
+            output['username'] = username
+                
+        # fall through if errors
+        return render_to(request, 'administrator/register.html', output)
 
 
 ###########################################################################
